b79ad6d0cd78ec8eb0b3e63763336ffd9c43ded9
yydzxz <>
2020-09-15 12:09:21 +0800

    feat: 字节跳动v2预授权码接口

--- src/main/java/com/github/yydzxz/open/api/v2/IByteDanceOpenV2ComponentService.java
+++ src/main/java/com/github/yydzxz/open/api/v2/IByteDanceOpenV2ComponentService.java
@@ -1,6 +1,8 @@
 package com.github.yydzxz.open.api.v2;
 
 import com.github.yydzxz.open.api.v1.IByteDanceOpenComponentService;
+import com.github.yydzxz.open.api.v1.response.auth.GetPreAuthCodeResponse;
+import com.github.yydzxz.open.api.v2.request.auth.GetPreAuthCodeRequest;
 
 /**
  * @author yangyidian
@@ -12,4 +14,7 @@ public interface IByteDanceOpenV2ComponentService extends IByteDanceOpenComponen
      * 获取预授权码 pre_auth_code
      */
     String V2_API_CREATE_PRE_AUTH_CODE_URL = "https://open.microapp.bytedance.com/openapi/v2/auth/pre_auth_code";
+
+
+    String getPreAuthUrl(String redirectURI, GetPreAuthCodeRequest request);
 }
