c73584f97e75239b219d9612f321b8d1a4dbb5f3
yydzxz <>
2020-11-18 10:07:45 +0800

    fix: "获取可选审核宿主端列表"接口返回内容映射对象失败

--- src/main/java/com/github/yydzxz/open/api/v1/response/code/CodeAuditHostsResponse.java
+++ src/main/java/com/github/yydzxz/open/api/v1/response/code/CodeAuditHostsResponse.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.yydzxz.common.error.ByteDanceError;
 import com.github.yydzxz.common.http.IByteDanceResponse;
 import com.google.gson.annotations.SerializedName;
+import java.io.Serializable;
 import java.util.List;
 import lombok.Data;
 
@@ -17,21 +18,31 @@ import lombok.Data;
 public class CodeAuditHostsResponse extends ByteDanceError implements IByteDanceResponse {
 
     private static final long serialVersionUID = -5630294923043007033L;
-    /**
-     * 本次审核可选宿主端英文简称
-     */
-    @SerializedName("hostNames")
-    @JSONField(name = "hostNames")
-    @JsonAlias("hostNames")
-    @JsonProperty("hostNames")
-    private List<String> hostNames;
 
-    /**
-     * 上次发布的宿主端英文简称
-     */
-    @SerializedName("releasedHostNames")
-    @JSONField(name = "releasedHostNames")
-    @JsonAlias("releasedHostNames")
-    @JsonProperty("releasedHostNames")
-    private List<String> releasedHostNames;
+    private DataObj data;
+
+    @Data
+    public static class DataObj implements Serializable {
+
+        private static final long serialVersionUID = 6679829880999868531L;
+
+        /**
+         * 本次审核可选宿主端英文简称
+         */
+        @SerializedName("hostNames")
+        @JSONField(name = "hostNames")
+        @JsonAlias("hostNames")
+        @JsonProperty("hostNames")
+        private List<String> hostNames;
+
+        /**
+         * 上次发布的宿主端英文简称
+         */
+        @SerializedName("releasedHostNames")
+        @JSONField(name = "releasedHostNames")
+        @JsonAlias("releasedHostNames")
+        @JsonProperty("releasedHostNames")
+        private List<String> releasedHostNames;
+    }
+
 }
