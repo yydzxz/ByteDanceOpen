351e498a735b002de4c1facc77f90d5cab9af662
yydzxz <>
2020-12-23 16:18:32 +0800

    申请状态返回值

--- src/main/java/com/github/yydzxz/open/api/v1/response/operation/OperationLiveApplicationStatusResponse.java
+++ src/main/java/com/github/yydzxz/open/api/v1/response/operation/OperationLiveApplicationStatusResponse.java
@@ -9,15 +9,22 @@ public class OperationLiveApplicationStatusResponse extends ByteDanceError imple
 
     private static final long serialVersionUID = -1351376418175751980L;
 
-    /**
-     * 「抖音直播组件」申请状态。
-     * 0：默认值，1：可申请，2：不可申请，3：申请中，4：申请通过，5：申请失败，6：能力关闭
-     */
-    private Integer status;
+    private DataObj data;
 
-    /**
-     * 「抖音直播组件」申请状态对应的原因
-     * 如果状态是0（默认值）、1（可申请）、3（申请中）、4（申请通过），则 reason 为空字符串
-     */
-    private String reason;
+    @Data
+    public static class DataObj{
+        /**
+         * 「抖音直播组件」申请状态。
+         * 0：默认值，1：可申请，2：不可申请，3：申请中，4：申请通过，5：申请失败，6：能力关闭
+         */
+        private Integer status;
+
+        /**
+         * 「抖音直播组件」申请状态对应的原因
+         * 如果状态是0（默认值）、1（可申请）、3（申请中）、4（申请通过），则 reason 为空字符串
+         */
+        private String reason;
+    }
+
+
 }
--- src/main/java/com/github/yydzxz/open/api/v1/response/operation/OperationVideoApplicationStatusResponse.java
+++ src/main/java/com/github/yydzxz/open/api/v1/response/operation/OperationVideoApplicationStatusResponse.java
@@ -9,15 +9,22 @@ public class OperationVideoApplicationStatusResponse extends ByteDanceError impl
 
     private static final long serialVersionUID = 5914133377456005753L;
 
-    /**
-     * 「短视频挂载」能力申请状态。
-     * 1：申请中，2：申请通过，3：申请失败，4：能力关闭，5：可申请，6：不可申请
-     */
-    private Integer status;
+    private DataObj data;
 
-    /**
-     * 「短视频挂载」能力申请状态对应的原因
-     * 如果状态是1（申请中）、2（申请通过）、5（可申请），则 reason 为空字符串
-     */
-    private String reason;
+    @Data
+    public static class DataObj{
+        /**
+         * 「短视频挂载」能力申请状态。
+         * 1：申请中，2：申请通过，3：申请失败，4：能力关闭，5：可申请，6：不可申请
+         */
+        private Integer status;
+
+        /**
+         * 「短视频挂载」能力申请状态对应的原因
+         * 如果状态是1（申请中）、2（申请通过）、5（可申请），则 reason 为空字符串
+         */
+        private String reason;
+    }
+
+
 }
