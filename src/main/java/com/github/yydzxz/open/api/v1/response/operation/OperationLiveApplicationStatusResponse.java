578cae8500375e0243dc3bc2c58733258689bb63
yydzxz <>
2020-12-23 16:13:52 +0800

    申请状态返回值

--- src/main/java/com/github/yydzxz/open/api/v1/response/operation/OperationLiveApplicationStatusResponse.java
+++ src/main/java/com/github/yydzxz/open/api/v1/response/operation/OperationLiveApplicationStatusResponse.java
@@ -8,4 +8,16 @@ import lombok.Data;
 public class OperationLiveApplicationStatusResponse extends ByteDanceError implements IByteDanceResponse {
 
     private static final long serialVersionUID = -1351376418175751980L;
+
+    /**
+     * 「抖音直播组件」申请状态。
+     * 0：默认值，1：可申请，2：不可申请，3：申请中，4：申请通过，5：申请失败，6：能力关闭
+     */
+    private Integer status;
+
+    /**
+     * 「抖音直播组件」申请状态对应的原因
+     * 如果状态是0（默认值）、1（可申请）、3（申请中）、4（申请通过），则 reason 为空字符串
+     */
+    private String reason;
 }
