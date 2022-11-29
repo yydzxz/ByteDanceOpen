afe08f545b87ca90911104681e60c4fb58279d05
yydzxz <79861784@qq.com>
2022-11-29 21:07:02 +0800

    bugfix

--- src/main/java/com/github/yydzxz/common/error/ByteDanceError.java
+++ src/main/java/com/github/yydzxz/common/error/ByteDanceError.java
@@ -47,6 +47,6 @@ public class ByteDanceError implements IByteDanceError, Serializable {
 
     @Override
     public String errorMessage() {
-        return errorMessage();
+        return getMessage();
     }
 }
